4f0d8d628a2e4e5de8de7395ef7cc0dfa1a0cf93
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-08 08:11:07 +0200

    :lipstick: Update color scheme
    Still not done...

--- lib/core/theme/app_color.dart
+++ lib/core/theme/app_color.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
+  // TODO: Change the color scheme to match the app's branding
   static final ColorScheme colorScheme = ColorScheme(
-    brightness: Brightness.dark,
-    primary: Color(0xFFFF6A00),
-    onPrimary: Colors.white,
-    secondary: Color(0xFF0095FF),
+    brightness: Brightness.light,
+    primary: Color(0xFF62b4b1),
+    onPrimary: Color(0xFF212121),
+    primaryContainer: Color(0xFF00BCD4),
+    secondary: Color(0xFF00BCD4),
     onSecondary: Colors.white,
     error: Colors.red,
     onError: Colors.white,
-    surface: Colors.grey[900]!,
+    surface: Colors.white,
+    primaryFixed: Colors.grey[900]!,
     onSurface: Colors.white,
     surfaceContainer: Colors.grey[800]!,
-    secondaryContainer: Color(0xFFFF6A00),
+    secondaryContainer: Color(0xFF3e7472),
   );
 }

0673152a0f458969039b52b312481e488f65f163
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-08 08:12:44 +0200

    :recycle: Rename MyApp to DebbyDebs and update home widget to HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:debbydebs/core/theme/app_color.dart';
-import 'package:debbydebs/ui/home/home_view.dart';
+import 'package:debbydebs/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const DebbyDebs());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DebbyDebs extends StatelessWidget {
+  const DebbyDebs({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: AppColor.colorScheme),
-      home: const HomeView(),
+      title: 'Debby Debs',
+      theme: ThemeData.from(colorScheme: AppColor.colorScheme),
+      home: const HomeScreen(),
     );
   }
 }

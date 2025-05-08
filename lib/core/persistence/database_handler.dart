91f53877210f4d87557ce56524274ca28a1bef94
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-08 18:21:36 +0200

    :card_file_box: Add debts table to database schema

--- lib/core/persistence/database_handler.dart
+++ lib/core/persistence/database_handler.dart
@@ -19,6 +19,9 @@ class DatabaseHandler {
     _database = await openDatabase(
       join(await getDatabasesPath(), 'app.db'),
       onCreate: (db, version) {
+        db.execute(
+          "CREATE TABLE debts(id INTEGER PRIMARY KEY, name TEXT, description TEXT, contactId INTEGER, amount REAL, isPaid INTEGER)",
+        );
         return db.execute(
           "CREATE TABLE contacts(id INTEGER PRIMARY KEY, name TEXT)",
         );

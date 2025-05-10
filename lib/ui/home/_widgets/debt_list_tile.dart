296c46da54366200a835e504c44700778ff92da7
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-10 22:35:57 +0200

    :lipstick: Refactor DebtListTile to use Card widget and add Edit button with improved layout

--- lib/ui/home/_widgets/debt_list_tile.dart
+++ lib/ui/home/_widgets/debt_list_tile.dart
@@ -10,7 +10,6 @@ class DebtListTile extends StatefulWidget {
   final int contactId;
   final double amount;
   final bool isPaid;
-
   final Function(int id) onTap;
   final Function(int id) onDelete;
 
@@ -37,61 +36,104 @@ class _DebtListTileState extends State<DebtListTile> {
       create: (_) => DebtListTileViewModel(contactId: widget.contactId),
       child: Consumer<DebtListTileViewModel>(
         builder: (context, viewModel, child) {
-          return Column(
-            children: [
-              ListTile(
-                textColor: Colors.black,
-                leading: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Initicon(text: viewModel.contactName, size: 30),
-                    Text(viewModel.contactName),
-                  ],
-                ),
-                title: Text(widget.name),
-                subtitle: Text(widget.description),
-                trailing: Text(
-                  "${widget.amount.toStringAsFixed(2)} €",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: widget.amount > 0 ? Colors.green : Colors.red,
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Column(
+              children: [
+                ListTile(
+                  textColor: Colors.black,
+                  leading: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      viewModel.showButtons
+                          ? const Icon(Icons.arrow_drop_up)
+                          : const Icon(Icons.arrow_drop_down),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Initicon(text: viewModel.contactName, size: 30),
+                          Text(viewModel.contactName),
+                        ],
+                      ),
+                    ],
                   ),
+                  title: Text(widget.name),
+                  subtitle: Text(widget.description),
+                  trailing: Text(
+                    "${widget.amount.toStringAsFixed(2)} €",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: widget.amount > 0 ? Colors.green : Colors.red,
+                    ),
+                  ),
+                  onTap: viewModel.toggleButtons,
                 ),
-                onTap: viewModel.toggleButtons,
-              ),
-              if (viewModel.showButtons)
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    FilledButton(
-                      onPressed: () => widget.onDelete(widget.id),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Icon(Icons.paid, color: Colors.white),
-                          Text(
-                            "Mark as Paid",
-                            style: TextStyle(color: Colors.white),
+                if (viewModel.showButtons) Divider(color: Colors.grey),
+                if (viewModel.showButtons)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        FilledButton(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.all(
+                              Colors.green,
+                            ),
                           ),
-                        ],
-                      ),
+                          onPressed: () => widget.onDelete(widget.id),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Icon(Icons.paid, color: Colors.white),
+                              Text(
+                                "Mark as Paid",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                        FilledButton(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.all(
+                              Colors.orange,
+                            ),
+                          ),
+                          onPressed: () => throw UnimplementedError(),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Icon(Icons.edit, color: Colors.white),
+                              Text(
+                                "Edit",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                        FilledButton(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.all(
+                              Colors.red,
+                            ),
+                          ),
+                          onPressed: () => widget.onDelete(widget.id),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(Icons.delete, color: Colors.white),
+                              Text(
+                                "Delete",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    FilledButton(
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(Colors.red),
-                      ),
-                      onPressed: () => widget.onDelete(widget.id),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Icon(Icons.delete, color: Colors.white),
-                          Text("Delete", style: TextStyle(color: Colors.white)),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-            ],
+                  ),
+              ],
+            ),
           );
         },
       ),

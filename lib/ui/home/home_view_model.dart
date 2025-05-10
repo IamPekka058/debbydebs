90f9704c96fb69bd9c24983b73f2c092edd55ba1
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-10 16:35:23 +0200

    refactor: Update database handler methods in HomeViewModel

--- lib/ui/home/home_view_model.dart
+++ lib/ui/home/home_view_model.dart
@@ -1,26 +1,31 @@
 import 'package:debbydebs/core/models/debt.dart';
-import 'package:debbydebs/core/persistence/database_handler.dart';
 import 'package:flutter/cupertino.dart';
+
+import '../../core/persistence/debt_database_handler.dart';
 
 class HomeViewModel extends ChangeNotifier {
   final List<Debt> _debts = [];
   List<Debt> get debts => _debts;
-  final DatabaseHandler _databaseHandler = DatabaseHandler();
+  final DebtDatabaseHandler _databaseHandler = DebtDatabaseHandler();
+
+  HomeViewModel() {
+    getAllDebts();
+  }
 
   void addDebt(Debt debt) {
-    _databaseHandler.insertDebt(debt);
+    _databaseHandler.insert(debt);
     _debts.add(debt);
     notifyListeners();
   }
 
   void removeDebt(Debt debt) {
-    _databaseHandler.deleteDebt(debt.id);
+    _databaseHandler.delete(debt);
     _debts.remove(debt);
     notifyListeners();
   }
 
   void getAllDebts() async {
-    final List<Debt> debts = await _databaseHandler.getDebts();
+    final List<Debt> debts = await _databaseHandler.getAll();
     _debts.clear();
     _debts.addAll(debts);
     notifyListeners();

6e160748e2e52493519168f42547962b6a1c7451
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-11 11:21:56 +0200

    :lipstick: Improve ContactsView with contact creation and listing functionality

--- lib/ui/contacts/contacts_view.dart
+++ lib/ui/contacts/contacts_view.dart
@@ -1,10 +1,47 @@
+import 'package:debbydebs/ui/contacts/_widgets/contact_list_tile.dart';
+import 'package:debbydebs/ui/contacts/contacts_view_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '_widgets/contact_creation_tile.dart';
 
 class ContactsView extends StatelessWidget {
   const ContactsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ChangeNotifierProvider(
+      create: (_) => ContactViewViewModel(),
+      child: Consumer<ContactViewViewModel>(
+        builder: (context, model, value) {
+          return Scaffold(
+            body: Column(
+              children: [
+                ContactCreationTile(onCreate: model.getContacts),
+                Divider(
+                  color: Theme.of(context).colorScheme.primary,
+                  thickness: 5,
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: model.contacts.length,
+                    itemBuilder: (context, index) {
+                      final contact = model.contacts[index];
+                      return ContactListTile(
+                        contactId: contact.id,
+                        contactName: contact.name,
+                        onEdit: (int id) {},
+                        onDelete: (int id) {},
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }

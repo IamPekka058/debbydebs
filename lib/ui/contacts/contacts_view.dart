c1d706d384dd357a37b87369370b8765288b8a05
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-05-28 23:24:59 +0200

    :art: Dart format

--- lib/ui/contacts/contacts_view.dart
+++ lib/ui/contacts/contacts_view.dart
@@ -10,57 +10,56 @@ class ContactsView extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) => ChangeNotifierProvider(
-      create: (_) => ContactViewViewModel(),
-      child: Consumer<ContactViewViewModel>(
-        builder: (final context, final viewModel, final value) {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            if (viewModel.errorOccurred) {
-              ScaffoldMessenger.of(context).clearSnackBars();
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text(
-                    "You cannot delete this contact because it is linked to a debt.",
-                  ),
-                  backgroundColor: Colors.red,
-                  showCloseIcon: true,
-                  duration: Duration(seconds: 2),
+    create: (_) => ContactViewViewModel(),
+    child: Consumer<ContactViewViewModel>(
+      builder: (final context, final viewModel, final value) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          if (viewModel.errorOccurred) {
+            ScaffoldMessenger.of(context).clearSnackBars();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text(
+                  "You cannot delete this contact because it is linked to a debt.",
                 ),
-              );
-              viewModel.resetError();
-            }
-          });
-          return Scaffold(
-            body: Column(
-              children: [
-                ContactCreationTile(onCreate: viewModel.getContacts),
-                Divider(
-                  color: Theme.of(context).colorScheme.primary,
-                  thickness: 2,
+                backgroundColor: Colors.red,
+                showCloseIcon: true,
+                duration: Duration(seconds: 2),
+              ),
+            );
+            viewModel.resetError();
+          }
+        });
+        return Scaffold(
+          body: Column(
+            children: [
+              ContactCreationTile(onCreate: viewModel.getContacts),
+              Divider(
+                color: Theme.of(context).colorScheme.primary,
+                thickness: 2,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: viewModel.contactsCount,
+                  itemBuilder: (final context, final index) {
+                    final Contact contact = viewModel.contacts[index];
+                    return ContactListTile(
+                      contactId: contact.id,
+                      contactName: contact.name,
+                      onEdit: (_) {},
+                      onDelete: viewModel.deleteContact,
+                      isSelected:
+                          viewModel.selectedContact != null &&
+                          viewModel.selectedContact!.id == contact.id,
+                      onSelect: () => viewModel.toggleContactSelection(contact),
+                    );
+                  },
                 ),
-                Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: viewModel.contactsCount,
-                    itemBuilder: (final context, final index) {
-                      final Contact contact = viewModel.contacts[index];
-                      return ContactListTile(
-                        contactId: contact.id,
-                        contactName: contact.name,
-                        onEdit: (_) {},
-                        onDelete: viewModel.deleteContact,
-                        isSelected:
-                            viewModel.selectedContact != null &&
-                            viewModel.selectedContact!.id == contact.id,
-                        onSelect:
-                            () => viewModel.toggleContactSelection(contact),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
+              ),
+            ],
+          ),
+        );
+      },
+    ),
+  );
 }

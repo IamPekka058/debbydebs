4d535adbf3b80bcbe992d6f51ffb54ebc28510b3
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2025-08-13 19:34:57 +0200

    :art: Update color scheme to align with app branding

--- lib/core/theme/app_color.dart
+++ lib/core/theme/app_color.dart
@@ -4,16 +4,16 @@ class AppColor {
   // TODO(IamPekka058): Change the color scheme to match the app's branding
   static final ColorScheme colorScheme = ColorScheme(
     brightness: Brightness.light,
-    primary: const Color(0xFF62b4b1),
-    onPrimary: const Color(0xFF212121),
+    primary: const Color(0xFF00B8A9),
+    onPrimary: Colors.white,
     primaryContainer: const Color(0xFF00BCD4),
-    secondary: const Color(0xFF00BCD4),
-    onSecondary: Colors.white,
-    error: Colors.red,
+    secondary: const Color(0xFF90E0D0),
+    onSecondary: Colors.black,
+    error: const Color(0xFFE63946),
     onError: Colors.white,
-    surface: Colors.white,
-    primaryFixed: Colors.grey[900],
+    surface: const Color(0xFFF6F9F9),
     onSurface: Colors.black,
+    primaryFixed: Colors.grey[900],
     surfaceContainer: Colors.grey[800],
     secondaryContainer: const Color(0xFF3e7472),
   );
